7367e5877b700d18d461c13261bddcb5ec9422f2
Amir Hossein <amirzadeh@live.com>
2024-10-24 23:12:43 +0330

    Config website app url and settings

--- core/core/urls.py
+++ core/core/urls.py
@@ -23,8 +23,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('website.urls')),
     path('blog/', include('blog.urls')),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
 ]
 
 if settings.DEBUG:
