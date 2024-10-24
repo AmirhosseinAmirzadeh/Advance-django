91524baf8c4b046bcf45c83aaa2aa9ef2928bbc4
Amir Hossein <amirzadeh@live.com>
2024-10-24 13:25:28 +0330

    Template View

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import indexView
 from django.views.generic import TemplateView
+from blog import views
 
 urlpatterns = [
     path('fbv-index/', indexView, name='fbv-index'),
-    path('cbv-index/', TemplateView.as_view(template_name='blog/index.html', extra_context={"name":"ali"})),
+    # path('cbv-index/', TemplateView.as_view(template_name='blog/index.html', extra_context={"name":"ali"})),
+    path('cbv-index/', views.IndexView.as_view(), name='cbv-index')
 ]
