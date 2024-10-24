03f76102ed49a874b63e943f0b0abf9d9fc75964
Amir Hossein <amirzadeh@live.com>
2024-10-24 11:54:44 +0330

    Function Based View (FBV)
    I just create a view and url for fbv-index as an example.

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-
+from .views import indexView
 
 urlpatterns = [
-    
+    path('fbv-index/', indexView, name='fbv-index'),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def indexView(request):
+    """
+    A function based view to show index page
+    """
+    name = "amirhossein"
+    context = {"name":name}
+    return render(request, 'index.html', context)
