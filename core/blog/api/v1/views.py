9faa56f56b36d05528731da385b8064102301521
Amir Hossein <amirzadeh@live.com>
2024-11-22 18:44:55 +0330

    PostList by API View

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -6,7 +6,9 @@ from django.shortcuts import get_object_or_404
 
 @api_view()
 def postList(request):
-    return Response("Hello AmirHossein")
+    posts = Post.objects.filter(status=True)
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
 
 
 @api_view()
