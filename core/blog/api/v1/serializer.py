9ab710d6a24a4f75ed54f172369af0e1e10ffd93
Amir Hossein <amirzadeh@live.com>
2025-03-02 01:53:12 +0330

    ReadOnly Fields
    Now you can read title or content but you can't see it for `put/patch` in `HTML form`.

--- core/blog/api/v1/serializer.py
+++ core/blog/api/v1/serializer.py
@@ -10,7 +10,7 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['id', 'author', 'title', 'content', 'status', 'created_date', 'published_date']
-        
+        read_only_fields = ['author']
         
 
 class CategorySerializer(serializers.ModelSerializer):
