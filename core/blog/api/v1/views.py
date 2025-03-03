62a7539ae7ba4ff5b8a798b221bfe42144825ba2
Amir Hossein <amirzadeh@live.com>
2025-03-04 00:05:43 +0330

    Order filter

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -11,7 +11,7 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 from .permissions import IsOwnerOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 @api_view(["GET", "POST"])
 def postList(request):
@@ -210,9 +210,10 @@ class PostModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['category', 'author']
     search_fields = ['title', 'content']
+    ordering_fields = ['published_date']
 
     @action(methods=['get'], detail=False)
     def get_ok(self, request):
