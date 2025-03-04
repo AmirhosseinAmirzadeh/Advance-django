1b557277978ae4883ae6a98c1b238efe1ce56b31
Amir Hossein <amirzadeh@live.com>
2025-03-04 09:34:21 +0330

    Documentation (CoreApiAutoSchema)

--- core/core/urls.py
+++ core/core/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 # static and media
 from django.conf import settings
 from django.conf.urls.static import static
-
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,6 +28,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    path('api-docs/', include_docs_urls(title='api sample'))
 ]
 
 if settings.DEBUG:
